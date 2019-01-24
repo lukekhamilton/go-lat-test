294fc5d5ce240e7ab04be06810cbe066646204a5
Luke Hamilton <luke@mettadata.com>
2019-01-24 13:01:59 +1100

    ALgo fix to work deal with time series data

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
 func getMaxProfit(stockPrices []int) int {
 	var lowestPrice int
 	var highestPrice int
+	var lowestPriceIndex int
+	var highestPriceIndex int
 
-	for _, price := range stockPrices {
-		if price < lowestPrice || lowestPrice == 0 {
+	for i, price := range stockPrices {
+		if (price < lowestPrice || lowestPrice == 0) && i != len(stockPrices)-1 {
 			lowestPrice = price
-		}
-		if price > highestPrice {
+			lowestPriceIndex = i
+			highestPrice = 0
+		} else if (price > highestPrice) && (i > lowestPriceIndex) {
 			highestPrice = price
+			highestPriceIndex = i
 		}
 	}
 
-	log.Printf("Lowest: %v, Highest: %v\n", lowestPrice, highestPrice)
+	log.Printf("Lowest[%v]: %v, Highest[%v]: %v\n", lowestPriceIndex, lowestPrice, highestPriceIndex, highestPrice)
 	return highestPrice - lowestPrice
 }
